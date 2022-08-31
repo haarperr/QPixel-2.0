9246e22236f0363a4237b3b56bcfd8e1c71479f3
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-08-31 20:35:51 +0100

    Fixed loading screen

--- resources/[desirerp-resources]/desirerp-loading-screen/fxmanifest.lua
+++ resources/[desirerp-resources]/desirerp-loading-screen/fxmanifest.lua
@@ -1,40 +1,15 @@
 fx_version 'cerulean'
-games { 'gta5' }
-
-loadscreen 'index.html'
-loadscreen_manual_shutdown 'yes'
-client_script 'client.lua'
+games {'gta5'}
 
 files {
     'index.html',
-    'css/bootstrap.min.css',
-    'css/css.css',
-    'css/fontawesome-all.min.css',
-    'css/colors/yellow-2.css',
-    'css/style-dark.css',
-    'img/film-grain.png',
-    'img/logo-light.png',
-    'js/main.js',
-    'js/plugins.js',
-    'webfonts/fa-brands-400.eot',
-    'webfonts/fa-brands-400.svg',
-    'webfonts/fa-brands-400.ttf',
-    'webfonts/fa-brands-400.woff',
-    'webfonts/fa-brands-400.woff2',
-    'webfonts/fa-light-300.eot',
-    'webfonts/fa-light-300.svg',
-    'webfonts/fa-light-300.ttf',
-    'webfonts/fa-light-300.woff',
-    'webfonts/fa-light-300.woff2',
-    'webfonts/fa-regular-400.eot',
-    'webfonts/fa-regular-400.svg',
-    'webfonts/fa-regular-400.ttf',
-    'webfonts/fa-regular-400.woff',
-    'webfonts/fa-regular-400.woff2',
-    'webfonts/fa-solid-900.eot',
-    'webfonts/fa-solid-900.svg',
-    'webfonts/fa-solid-900.ttf',
-    'webfonts/fa-solid-900.woff',
-    'webfonts/fa-solid-900.woff2',
-    'img/ic.png'
+    'style.css',
+    'script.js',
+    'videos/*',
 }
+
+loadscreen 'index.html'
+
+loadscreen_manual_shutdown "yes"
+
+client_script "client.lua"
