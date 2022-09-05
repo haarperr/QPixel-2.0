c866ef7f397efc9dea3ab03d5902d06add2cd739
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-09-05 17:49:34 +0100

    New misc files.

--- resources/[desirerp-streamed]/[desirerp-extrastream]/desirerp-misc/fxmanifest.lua
+++ resources/[desirerp-streamed]/[desirerp-extrastream]/desirerp-misc/fxmanifest.lua
@@ -9,8 +9,4 @@ files {
 
 data_file 'POPSCHED_FILE' 'popcycle.dat'
 data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/np_misc_load.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/np_misc.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/interior_props/discoverybuilder.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/greenwarehouse/warehouse_gnp_milo_.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/pride_month/pride_month.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/Chessboard/np_chess.ytyp'
