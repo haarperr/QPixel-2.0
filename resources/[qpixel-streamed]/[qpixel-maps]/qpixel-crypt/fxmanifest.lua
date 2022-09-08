e3d2fa8ec406a4c37af7cdead01c327dd975a863
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-09-08 03:41:40 +0100

    2

---
+++ resources/[qpixel-streamed]/[qpixel-maps]/qpixel-crypt/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Xee'
+description 'crypt'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'xee_crypt_timecycle.xml'
+
+files {
+    'xee_crypt_timecycle.xml'
+}
