00ddfea7e328c511f8199ba4270d037cdcb554b7
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-09-09 04:38:36 +0100

    sd

--- resources/[qpixel-resources]/qpixel-weed/fxmanifest.lua
+++ resources/[qpixel-resources]/qpixel-weed/fxmanifest.lua
@@ -1,29 +1,33 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-client_script "@arp-sync/client/lib.lua"
-client_script "@arp-sync/client/lib.lua"
-client_script '@arp-lib/client/cl_interface.lua'
-client_script "@arp-lib/client/cl_polyhooks.lua"
+client_script "@qpixel-sync/client/lib.lua"
+client_script "@qpixel-sync/client/lib.lua"
+client_script '@qpixel-lib/client/cl_interface.lua'
+client_script "@qpixel-lib/client/cl_polyhooks.lua"
 client_script "@mka-array/Array.lua"
-client_script "@arp-lib/client/cl_ui.lua"
-client_script "@arp-lib/client/cl_rpc.lua"
+client_script "@qpixel-lib/client/cl_ui.lua"
+client_script "@qpixel-lib/client/cl_rpc.lua"
 
 client_scripts {
-  '@arp-lib/client/cl_rpc.lua',
+  "@qpixel-sync/client/lib.lua",
+  "@qpixel-lib/client/cl_interface.lua",
+  "@qpixel-lib/client/cl_polyhooks.lua",
+  "@qpixel-locales/client/lib.lua",
+  "@qpixel-lib/client/cl_rpc.lua",
   'client/cl_*.lua',
   "@PolyZone/client.lua",
 }
 
 shared_script {
-  '@arp-lib/shared/sh_util.lua',
+  '@qpixel-lib/shared/sh_util.lua',
   'shared/sh_*.*',
 }
 
 server_scripts {
-  '@arp-lib/server/sv_rpc.lua',
-  '@arp-lib/server/sv_sql.lua',
-  '@arp-lib/server/sv_asyncExports.lua',
+  "@qpixel-lib/server/sv_rpc.lua",
+  "@qpixel-lib/server/sv_sql.lua",
+  "@qpixel-lib/server/sv_asyncExports.lua",
   'server/sv_*.lua',
 }
 
