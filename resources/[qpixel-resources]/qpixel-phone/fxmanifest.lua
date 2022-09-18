81278363dd4f7652235dde7371eb5045d38ffe3f
loleris-pp <108324559+loleris-pp@users.noreply.github.com>
2022-09-18 12:52:37 -0400

    f

--- resources/[qpixel-resources]/qpixel-phone/fxmanifest.lua
+++ resources/[qpixel-resources]/qpixel-phone/fxmanifest.lua
@@ -13,6 +13,7 @@ files {
 	'html/css/materialize.min.css',
 	'html/css/*.css',
 	'html/js/phone/*.js',
+	'html/js/phone/apps/*.js',
 	'html/images/*.png',
 	'html/images/*.jpg',
 	'html/images/*.svg',
