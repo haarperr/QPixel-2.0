56d7a0349bef0233be02c90f06fcf1d12fef8879
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-10-24 00:56:59 +0100

    Sell gems

--- resources/[qpixel-resources]/qpixel-jobs/client/cl_gallery.lua
+++ resources/[qpixel-resources]/qpixel-jobs/client/cl_gallery.lua
@@ -166,9 +166,9 @@ RegisterInterfaceCallback('gallery_8ct', function(data, cb)
     TriggerEvent('qpixel-jobs:gallery_sell_8ct_chains')
 end)
 
--- Sales
+ --Sales
 
--- Mining Gem
+ --Mining Gem
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_diamonds')
 AddEventHandler('qpixel-jobs:gellery_sell_diamonds', function()
@@ -191,21 +191,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryGem', function(data, cb)
         local GemAmount = data.values.pGemAmount
         if exports['qpixel-inventory']:hasEnoughOfItem('mineddiamond', GemAmount) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*GemAmount, 'Selling Diamond Gems')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*GemAmount, 'Selling Diamond Gems')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('mineddiamond', GemAmount) then
                 TriggerEvent('inventory:removeItem', 'mineddiamond', GemAmount)
                 TriggerServerEvent('qpixel-financials:business_money', 200*GemAmount, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Mining Stone
+ --Mining Stone
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_aquamarine')
 AddEventHandler('qpixel-jobs:gellery_sell_aquamarine', function()
@@ -229,21 +229,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryStone', function(data, cb)
 
     if exports['qpixel-inventory']:hasEnoughOfItem('minedaquamarine', pAquaAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(250*pAquaAmt, 'Selling Aquamarine')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(250*pAquaAmt, 'Selling Aquamarine')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('minedaquamarine', pAquaAmt) then
                 TriggerEvent('inventory:removeItem', 'minedaquamarine', pAquaAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 555*pAquaAmt, 'gallery', 'add')
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Mining Jade
+ --Mining Jade
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_jade')
 AddEventHandler('qpixel-jobs:gellery_sell_jade', function()
@@ -267,21 +267,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryJade', function(data, cb)
 
     if exports['qpixel-inventory']:hasEnoughOfItem('minedjade', pJadeAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pJadeAmt, 'Selling Mined Jade')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pJadeAmt, 'Selling Mined Jade')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('minedjade', pJadeAmt) then
                 TriggerEvent('inventory:removeItem', 'minedjade', pJadeAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 877*pJadeAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Mining Citrine
+ --Mining Citrine
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_citrine')
 AddEventHandler('qpixel-jobs:gellery_sell_citrine', function()
@@ -304,21 +304,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryCitrine', function(data, cb)
     local pCitrineAmt = data.values.pCitrineAmount
     if exports['qpixel-inventory']:hasEnoughOfItem('minedcitrine', pCitrineAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2500*pCitrineAmt, 'Selling Citrine')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2500*pCitrineAmt, 'Selling Citrine')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('minedcitrine', pCitrineAmt) then
                 TriggerEvent('inventory:removeItem', 'minedcitrine', pCitrineAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 1925*pCitrineAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Mining Garnet
+ --Mining Garnet
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_garnet')
 AddEventHandler('qpixel-jobs:gellery_sell_garnet', function()
@@ -341,16 +341,16 @@ RegisterInterfaceCallback('qpixel-jobs:galleryGarnet', function(data, cb)
     local pGarnetAmt = data.values.pGarnetAmount
         if exports['qpixel-inventory']:hasEnoughOfItem('minedgarnet', pGarnetAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pGarnetAmt, 'Selling Garnet')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pGarnetAmt, 'Selling Garnet')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('minedgarnet', pGarnetAmt) then
                 TriggerEvent('inventory:removeItem', 'minedgarnet', pGarnetAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 1650*pGarnetAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
-            -- else
-            --     FreezeEntityPosition(PlayerPedId(), false)
-            --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-            -- end
+             else
+                 FreezeEntityPosition(PlayerPedId(), false)
+                 TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+             end
         end
     end
 end)
@@ -378,21 +378,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryOpal', function(data, cb)
     local pOpalAmt = data.values.pOpalAmount
         if exports['qpixel-inventory']:hasEnoughOfItem('minedopal', pOpalAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pOpalAmt, 'Selling Opal')
-        -- if finished == 100 then
+        local finished = exports['qpixel-taskbar']:taskBar(2000*pOpalAmt, 'Selling Opal')
+        if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('minedopal', pOpalAmt) then
                 TriggerEvent('inventory:removeItem', 'minedopal', pOpalAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 580*pOpalAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+        else
+            FreezeEntityPosition(PlayerPedId(), false)
+            TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+        end
     end
 end)
 
--- Stolen Art
+ --Stolen Art
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_stolen_art')
 AddEventHandler('qpixel-jobs:gellery_sell_stolen_art', function()
@@ -415,21 +415,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryArt', function(data, cb)
     local pArtAmt = data.values.pArtAmount
         if exports['qpixel-inventory']:hasEnoughOfItem('stolenart', pArtAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pArtAmt, 'Selling Art Pieces')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pArtAmt, 'Selling Art Pieces')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('stolenart', pArtAmt) then
                 TriggerEvent('inventory:removeItem', 'stolenart', pArtAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 1500*pArtAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Golden Coins
+ --Golden Coins
 
 RegisterNetEvent('qpixel-jobs:gallery_sell_gold_coins')
 AddEventHandler('qpixel-jobs:gallery_sell_gold_coins', function()
@@ -452,21 +452,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryGoldcoin', function(data, cb)
     local pGoldCointAmt = data.values.pGoldCoinAmount
         if exports['qpixel-inventory']:hasEnoughOfItem('goldcoin', pGoldCointAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pGoldCointAmt, 'Selling Golden Coins')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pGoldCointAmt, 'Selling Golden Coins')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('goldcoin', pGoldCointAmt) then
                 TriggerEvent('inventory:removeItem', 'goldcoin', pGoldCointAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 85*pGoldCointAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Valuable Goods
+ --Valuable Goods
 
 RegisterNetEvent('qpixel-jobs:gallery_sell_val_goods')
 AddEventHandler('qpixel-jobs:gallery_sell_val_goods', function()
@@ -489,21 +489,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryVG', function(data, cb)
     local pVGAmt = data.values.pVGAmount
         if exports['qpixel-inventory']:hasEnoughOfItem('valuablegoods', pVGAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pVGAmt, 'Selling Valuable Goods')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pVGAmt, 'Selling Valuable Goods')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('valuablegoods', pVGAmt) then
                 TriggerEvent('inventory:removeItem', 'valuablegoods', pVGAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 352*pVGAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
-            -- else
-            --     FreezeEntityPosition(PlayerPedId(), false)
-            --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-            -- end
+             else
+                 FreezeEntityPosition(PlayerPedId(), false)
+                 TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+             end
         end
     end 
 end)
 
--- Golden Bars
+ --Golden Bars
 
 RegisterNetEvent('qpixel-jobs:gallery_sell_gold_bars')
 AddEventHandler('qpixel-jobs:gallery_sell_gold_bars', function()
@@ -526,21 +526,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryGoldBar', function(data, cb)
     local pGoldAmt = data.values.pGoldAmount
     if exports['qpixel-inventory']:hasEnoughOfItem('goldbar', pGoldAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pGoldAmt, 'Selling Valuable Goods')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pGoldAmt, 'Selling Valuable Goods')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('goldbar', pGoldAmt) then
                 TriggerEvent('inventory:removeItem', 'goldbar', pGoldAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 2000*pGoldAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
-            -- else
-            --     FreezeEntityPosition(PlayerPedId(), false)
-            --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-            -- end
+             else
+                 FreezeEntityPosition(PlayerPedId(), false)
+                 TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+             end
         end
     end
 end)
 
--- Rolex Watch
+ --Rolex Watch
 
 RegisterNetEvent('qpixel-jobs:gallery_sell_rolex_watch')
 AddEventHandler('qpixel-jobs:gallery_sell_rolex_watch', function()
@@ -563,21 +563,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryRolexWatch', function(data, cb)
     local pRolexAmt = data.values.pRolexAmount
     if exports['qpixel-inventory']:hasEnoughOfItem('rolexwatch', pRolexAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pRolexAmt, 'Selling Rolex Watche\'s')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pRolexAmt, 'Selling Rolex Watche\'s')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('rolexwatch', pRolexAmt) then
                 TriggerEvent('inventory:removeItem', 'rolexwatch', pRolexAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 114*pRolexAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- 8CT Chains
+ --8CT Chains
 
 RegisterNetEvent('qpixel-jobs:gallery_sell_8ct_chains')
 AddEventHandler('qpixel-jobs:gallery_sell_8ct_chains', function()
@@ -600,23 +600,23 @@ RegisterInterfaceCallback('qpixel-jobs:gallery8Ct', function(data, cb)
     local p8CTAmt = data.values.p8ctAmount
     if exports['qpixel-inventory']:hasEnoughOfItem('stolen8ctchain', p8CTAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*p8CTAmt, 'Selling Ruby\'s')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*p8CTAmt, 'Selling Ruby\'s')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('stolen8ctchain', p8CTAmt) then
                 TriggerEvent('inventory:removeItem', 'stolen8ctchain', p8CTAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 25*p8CTAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Ruby
+ --Ruby
 
--- Mining Ruby
+ --Mining Ruby
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_ruby')
 AddEventHandler('qpixel-jobs:gellery_sell_ruby', function()
@@ -639,21 +639,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryRuby', function(data, cb)
     local pRubyAmt = data.values.pRubyAmount
     if exports['qpixel-inventory']:hasEnoughOfItem('miningruby', pRubyAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pRubyAmt, 'Selling Ruby\'s')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pRubyAmt, 'Selling Ruby\'s')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('miningruby', pRubyAmt) then
                 TriggerEvent('inventory:removeItem', 'miningruby', pRubyAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 100*pRubyAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
             end
-        -- else
-        --     FreezeEntityPosition(PlayerPedId(), false)
-        --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-        -- end
+         else
+             FreezeEntityPosition(PlayerPedId(), false)
+             TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+         end
     end
 end)
 
--- Starry Night
+ --Starry Night
 
 RegisterNetEvent('qpixel-jobs:gellery_sell_night')
 AddEventHandler('qpixel-jobs:gellery_sell_night', function()
@@ -676,21 +676,21 @@ RegisterInterfaceCallback('qpixel-jobs:galleryNight', function(data, cb)
     local pStarryAmt = data.values.pStarryAmount 
     if exports['qpixel-inventory']:hasEnoughOfItem('starrynight', pStarryAmt) then
         FreezeEntityPosition(PlayerPedId(), true)
-        -- local finished = exports['qpixel-taskbar']:taskBar(2000*pStarryAmt, 'Selling Starry Night')
-        -- if finished == 100 then
+         local finished = exports['qpixel-taskbar']:taskBar(2000*pStarryAmt, 'Selling Starry Night')
+         if finished == 100 then
             if exports['qpixel-inventory']:hasEnoughOfItem('starrynight', pStarryAmt) then
                 TriggerEvent('inventory:removeItem', 'starrynight', pStarryAmt)
                 TriggerServerEvent('qpixel-financials:business_money', 550*pStarryAmt, 'gallery', 'add') 
                 FreezeEntityPosition(PlayerPedId(), false)
-            -- else
-            --     FreezeEntityPosition(PlayerPedId(), false)
-            --     TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
-            -- end
+             else
+                 FreezeEntityPosition(PlayerPedId(), false)
+                 TriggerEvent('DoLongHudText', 'Might wanna try again', 2)
+             end
         end
     end
 end)
 
--- // Eye // --
+ --// Eye // 
 
 exports["qpixel-polytarget"]:AddBoxZone("artgallerysell", vector3(-466.86, 46.12, 46.23), 1, 1, {
     heading=43,
